f2258d4b4eb853a689db42f5f0911cdec8bc6ec3
yrajeshtechsierra <rajesh.yadav@techsierra.in>
2024-03-26 13:56:58 +0530

    Set the Route of Home and Contact Page

---
+++ src/Pages/Home/home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../../Components/Header/Header";
+import Banner from "../../Components/Banner/Banner";
+import SectionThree from "../../Components/SectionThree/SectionThree";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <SectionThree />
+    </div>
+  );
+};
+
+export default Home;
