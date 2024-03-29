41779fe81c8bb42797c950741bbb3c6bd86f4350
yrajeshtechsierra <rajesh.yadav@techsierra.in>
2024-03-29 16:51:00 +0530

    Modified in the HTA

---
+++ src/Pages/Apply/HowToApply/HTAData.js
@@ -0,0 +1,9 @@
+import step1 from "../../../Assets/Images/Apply/step1.png";
+import step2 from "../../../Assets/Images/Apply/step2.png";
+import step3 from "../../../Assets/Images/Apply/step3.png";
+
+export const HTADATA = [
+  { id: 1, imgSrc: step1, text: "search job", path: "#FIXME" },
+  { id: 2, imgSrc: step2, text: "apply directly", path: "#FIXME" },
+  { id: 3, imgSrc: step3, text: "upload resume", path: "#FIXME" },
+];
