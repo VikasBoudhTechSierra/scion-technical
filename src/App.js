7ca7de53fa4168c047ce595a24b3197b0bf33dcb
yrajeshtechsierra <rajesh.yadav@techsierra.in>
2024-04-01 13:02:23 +0530

    updated the component

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ import IT from "./Pages/About/IT/IT";
 import ITSecurity from "./Pages/About/ITSecurity/ITSecurity";
 import StartUp from "./Pages/TemporaryStaffing/StartUp/StartUp";
 import Ai from "./Pages/TemporaryStaffing/Aitest/Ai";
-import Accounts from "./Pages/Apply/Accounts/Accounts";
 import HowToApply from "./Pages/Apply/HowToApply/HowToApply";
 import ItJobsInterview from "./Pages/Apply/ITJobsInterviewTips/ItJobsInterview";
 import SearchItJobs from "./Pages/Apply/Search_IT_JOBs/SearchItJobs";
 import PositionWeRecurit from "./Pages/Apply/PositionWeRecurit/PositionWeRecurit";
+import ApplyWithUs from "./Pages/Apply/ApplyWithUs/ApplyWithUs";
 function App() {
   return (
     <BrowserRouter>
@@ -47,7 +47,7 @@ function App() {
           exact
         />
         <Route path="/temporary-staffing/ai" element={<Ai />} exact />
-        <Route path="/apply/accounts" element={<Accounts />} exact />
+        <Route path="/apply/accounts" element={<ApplyWithUs />} exact />
         <Route path="/apply/how-to-apply" element={<HowToApply />} exact />
         <Route path="/apply/it-jobs" element={<ItJobsInterview />} exact />
         <Route path="/apply/search-it-jobs" element={<SearchItJobs />} exact />
---
+++ src/Pages/Apply/ApplyWithUs/ApplyWithUs.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ApplyWithUs = () => {
+  return <div>ApplyWithUs</div>;
+};
+
+export default ApplyWithUs;
